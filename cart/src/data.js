e2f19c4bdbfbcf1368eafb241f8c12312dbdf780
AnjanaDeepak7 <anjanadeepak60@gmail.com>
2024-07-12 09:56:54 +0530

    initial changes

---
+++ cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://cpng.pikpng.com/pngl/s/1-12595_download-samsung-mobile-phone-png-images-transparent-samsung.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel ",
+    price: 499.99,
+    img: "https://rukminim2.flixcart.com/image/850/1000/xif0q/mobile/a/s/x/-original-imagtwh4snkqfdam.jpeg?q=90&crop=false",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://rukminim2.flixcart.com/image/850/1000/k7w8eq80/mobile/z/f/6/mi-redmi-note-9-pro-b0784d862m-original-imafqf8ajpszacxz.jpeg?q=90&crop=false",
+    amount: 1,
+  },
+];
